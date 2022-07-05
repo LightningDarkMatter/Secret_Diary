712b3d13a5ad3c49f086cb9acd84ceabcd90d6bd
LightningDarkMatter <70206474+LightningDarkMatter@users.noreply.github.com>
2022-07-05 15:35:01 +0100

    Delete connection.php

--- connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-$link = mysqli_connect("address_of_db", "username", "password", "username");
-        if(mysqli_connect_error()) {
-            die("Error connecting to database");
-        }
-?>
